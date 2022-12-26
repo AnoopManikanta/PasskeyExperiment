0fb0f3f41fa9d092abbed555b739490ddbd68557
Anoop Manikanta <anoop.ms@surya-soft.com>
2022-12-26 10:36:26 +0530

    Initial Commit

---
+++ PasskeyExperiment/PasskeyExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasskeyExperimentApp.swift
+//  PasskeyExperiment
+//
+//  Created by Anoop Subramani on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PasskeyExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
